d5ae01036839b549c9d749dac8a7eeb123cf21b1
glitch-afk <rahul004@protonmail.ch>
2022-04-20 19:09:01 +0530

    :art: Added some theming and some packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        colors: {
+            "river-styx": "#121823",
+            "bright-greek": "#3C43EE",
+        },
         extend: {},
     },
     plugins: [],
